83be86a787be8e89e82083ea5b6b054cbde1485c
Sarah Hannes <78901374+SarahHannes@users.noreply.github.com>
2021-04-04 13:00:00 +0800

    Add files via upload

---
+++ Day03_history/03_history.R
@@ -0,0 +1,46 @@
+# Load libraries
+extrafont::loadfonts(device = "win", quiet=T)
+library(tidyverse)
+library(ggridges)
+library(ggstream)
+
+# Load data
+who <- read.csv("who_suicide_statistics.csv")
+
+# Preprocessing
+all <- who %>% 
+  group_by(age, year) %>%
+  mutate(total = sum(suicides_no, na.rm=T)) %>%
+  summarise(suicides_no, total) %>%
+  slice(1L) %>%
+  ungroup()
+
+# Plot
+p1 <- ggplot(data=all, aes(x=year, y=total, fill=age)) +
+  geom_stream() +
+  geom_stream_label(aes(label = age), family='Bahnschrift', size=3, color='gainsboro') +
+  scale_fill_brewer(palette = "Dark2") +
+  theme_ridges() +
+  theme(
+    legend.position = 'none',
+    panel.grid.major = element_blank(),
+    plot.background = element_rect(fill='#F4EEED', color='#F4EEED'),
+    panel.background = element_rect(fill='#F4EEED', color='#F4EEED'),
+    axis.title = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(size=6, color='darkgray')
+  )
+
+p <- p1 + 
+  labs(title=str_wrap("Historical View on the World's Suicide Occurrences by Age Group (1980 ~ 2016)", 50),
+       subtitle='Data: World Health Organization',
+       caption = "#30DayChartChallenge | @saraahannes") +
+  theme(
+    plot.title = element_text(family='Bahnschrift', color='darkgray', size=12.3),
+    plot.subtitle = element_text(family='IBahnschrift', color='darkgray', size=8, vjust=0),
+    plot.caption = element_text(family='Bahnschrift', hjust = 0.5, vjust = 0.5, size=4, color='darkgray')
+  )
+
+# Save plot
+ggsave("03_history.png", plot = p, type = 'cairo', width = 5, height = 3, dpi = 500, units = "in", bg='#fffeea')
