e3593d4725fbdc2f2098d410b38faaeca6b408e0
Sarah Hannes <78901374+SarahHannes@users.noreply.github.com>
2021-04-11 22:11:47 +0800

    Update 08_animal.R

--- Day08_animal/08_animal.R
+++ Day08_animal/08_animal.R
@@ -5,10 +5,9 @@ library(maps)
 library(ggfx)
 library(ggalt)
 library(ggforce)
-#library(RColorBrewer)
 library(magick)
-library(patchwork)
 
+# Palette & fonts
 font <- 'Bahnschrift'
 mycolors <- colorRampPalette(brewer.pal(8, "Set2"))(135) # expand color palettes
 mapcol <- c("#242424", "#B4B4B4", '#828282')
@@ -16,8 +15,12 @@ bgcol <- "#fcfcfc"
 pathcol <- '#a8b8d1'
 flightcol <- '#cccccc'
 
+# Load data
+# Data courtesy of Gabriel Preda & Xeno-Canto
+# Obtained from https://www.kaggle.com/gpreda/xenocanto-birds-from-romania
 bird <- read.csv("birds_romania.csv")
 
+# World map
 world <- map_data(map='world') %>%
   filter(region %in% c('Romania', 'Hungary', 'Bulgaria', 'Serbia', 'Moldova'))
 
@@ -27,23 +30,26 @@ regionmap1 <- world %>%
   summarise(long = mean(long), lat = max(lat)) %>%
   ungroup()
 
+# Image courtesy of clipart-library.com
 flight <- image_read("flight2.png") %>%
   image_background("none") %>%
   image_fill(flightcol, point = "+100+100", fuzz = 100)
 
+# Pre-processing
 bird <- bird %>%
   filter(lng!=0, lat!=0) %>%
   mutate(sp=factor(sp))
 
-
-
+# Plot
 p1 <- ggplot() +
-  
+  # Base map
   geom_polygon(data=world, aes(x=long, y=lat, group=group), fill=mapcol[1], color=mapcol[1]) +
   geom_polygon(data=world %>% filter(region!='Romania'), aes(x=long, y=lat, group=group), fill=mapcol[2], color=mapcol[2]) +
   
-  
+  # Label for Romania
   geom_text(data=regionmap1 %>% filter(region=='Romania'), aes(x=long, y=lat, label=region), color=mapcol[1], family=font) +
+  
+  # Bird flight path (with some glows) :D
   with_bloom(
     geom_link2(data=bird, aes(x=lng, y=lat, group=1, alpha=stat(index/100000)), lineend = 'round', color=pathcol),
     threshold_lower = 50,
@@ -52,21 +58,21 @@ p1 <- ggplot() +
     strength = 1,
     ) +
   
-  # title
+  # Title
   as_reference(
   geom_text(aes(x=30, y=39, label=str_wrap('Migratory Bird Trips in & around Romania', 30)),
             size = 10, hjust=1, family=font, lineheight=0.7),
     id='title') +
   
-  # bird image
+  # Bird image with some blending with title, but not too apparent lol ;)
   with_blend(
     annotation_raster(flight, xmin = 15, xmax = 25, ymin = 37, ymax = 40),
     bg_layer='title',
     blend_type="exclusion") +
   
+  # Subtitle
   geom_text(aes(x=30, y=37, label=str_wrap('Visualizing the Flight Paths of 135 Bird Species', 100)),
             size = 5, hjust=1, family=font, color=pathcol, lineheight=0.4) +
-  
   
   labs(caption='Data: Xeno-canto Foundation | #30DayChartChallenge | @saraahannes') +
   coord_cartesian(xlim=c(15,max(world$long)), ylim=c(33, max(world$lat))) +
@@ -76,7 +82,6 @@ p1 <- ggplot() +
     plot.background = element_rect(fill=bgcol, color=bgcol),
     plot.caption = element_text(family=font, hjust = 0.5, vjust=0.5, size=8, color=mapcol[2], margin = margin(-3, 0, 0.3, 0, "cm"))
   )
-
 
 # Save plot
 ggsave("08_animal.png", plot = p1, type = 'cairo', width = 5, height = 5, dpi = 300, units = "in", bg=bgcol)
